e574a251dffe8ba30402fc6b01facd7d4953248d
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-04-23 21:47:11 -0500

    ADD - Se añade nueva pantalla de consultar certificado.

---
+++ src/app/features/certificados/pages/certificados.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ConsultarCertificadoPage } from './consultar-certificado-page/consultar-certificado-page';
+
+export const CERTIFICADOS_ROUTES: Routes = [
+  { path: 'consultar-certificado', component: ConsultarCertificadoPage }
+];
--- src/app/features/certificados/pages/consultar-certificado-page/consultar-certificado-page.ts
+++ src/app/features/certificados/pages/consultar-certificado-page/consultar-certificado-page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../../shared/components/navbar/navbar';
+import { FooterComponent } from '../../../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-consultar-certificado-page',
-  imports: [],
+  imports: [
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './consultar-certificado-page.html',
   styleUrl: './consultar-certificado-page.scss',
 })
