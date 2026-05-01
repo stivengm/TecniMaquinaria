f2cefb2fa252dc26e9b59d3b76c501de6c19891a
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-04-30 23:27:25 -0500

    FIX - Ajuste para redireccionar desde Explorar cursos a la página de cursos

--- src/app/features/landing/pages/home-page/home-page.ts
+++ src/app/features/landing/pages/home-page/home-page.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar';
 import { FooterComponent } from '../../../../shared/components/footer/footer';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
   imports: [
     NavbarComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    RouterLink
+],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePageComponent {
 
   actualYear = new Date().getFullYear();
+
+  constructor(private router: Router) {
+    
+  }
+  
+  goToPage(link: string) {
+    this.router.navigate([link]);
+  }
 }
