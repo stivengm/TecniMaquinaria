dfb3ad194f8a367764617b84cdd69cca47ca3aee
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-03 21:16:09 -0500

    FIX - Ajuste para dejar la página de cursos completa con maquetación inicial.

--- src/app/features/landing/pages/home-page/home-page.ts
+++ src/app/features/landing/pages/home-page/home-page.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar';
 import { FooterComponent } from '../../../../shared/components/footer/footer';
-import { Router, RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
   imports: [
     NavbarComponent,
     FooterComponent,
-    RouterLink
 ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
@@ -17,9 +16,7 @@ export class HomePageComponent {
 
   actualYear = new Date().getFullYear();
 
-  constructor(private router: Router) {
-    
-  }
+  constructor(private router: Router) {}
   
   goToPage(link: string) {
     this.router.navigate([link]);
