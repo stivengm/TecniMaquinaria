45df323cb4bf2fa8fad118446157dd9701830a2a
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-11 19:27:03 -0500

    FIX - Ajuste de animaciones para la página de inicio.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
   protected title = 'TecniMaquinaria';
+
+  ngAfterViewInit() {
+    AOS.init({
+      duration: 800,
+      once: false,
+      mirror: true
+    });
+  }
 }
