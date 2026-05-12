188d535505e56135104a4858b7b67e00d6a35ca9
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-11 19:37:38 -0500

    ADD - Se implementa animación de máquina de escribir en el home.

--- src/app/features/landing/pages/home-page/home-page.ts
+++ src/app/features/landing/pages/home-page/home-page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar';
 import { FooterComponent } from '../../../../shared/components/footer/footer';
 import { DataAppServices } from '../../../../core/services/data-app';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home-page',
@@ -12,7 +13,7 @@ import { DataAppServices } from '../../../../core/services/data-app';
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
 
   actualYear = new Date().getFullYear();
 
@@ -20,6 +21,20 @@ export class HomePageComponent {
     private dataApp: DataAppServices
   ) {}
   
+  ngAfterViewInit(): void {
+    new Typed('#typing', {
+      strings: [
+        'de la industria',
+        'de la Retroexcavadora',
+        'de maquinaria Amarilla'
+      ],
+
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true
+    });
+  }
+
   goToPage(link: string) {
     this.dataApp.goToLink(link);
   }
